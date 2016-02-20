ba92c3cb4fccd5ae1ed353dcc21cd62b73954dc0
= <=>
2016-02-20 13:19:10 +0100

    PLACE WALL correct

--- QuoridorAlexJules/board.cpp
+++ QuoridorAlexJules/board.cpp
@@ -55,27 +55,21 @@ string Board::toString(){
 void Board::place(unsigned row, unsigned column, unsigned direction){  ;
     //Envisager cette condition afin de vérifier qu'on puisse placer un mur sans problème
     //Dans le cas d'un pion, traitement différent lié aux obstacles etc...
-    //row ne peut pas etre égal à
-    switch(direction){
-    case 0:
-        //Placement de pièce à l'horizontale à prévoir
-        plateau_[row][column]->place();
-        plateau_[row][column-1]->place();
-        plateau_[row][column+1]->place();
-        break;
-    case 1:
-        //Placement de pièce à la verticale à prévoir
-        plateau_[row][column]->place();
-        plateau_[row+1][column]->place();
-        plateau_[row-1][column]->place();
-
-        break;
-    default:
-        throw QuoridorExceptions(1,"Incorrect case of direction: Only 0 or 1",1);
-     }
-
-
+    //row ET column DOIVENT ETRE TOUT LES DEUX IMPAIRES !!!! +++
+     unsigned hidden_len=len_*2-1;
+     if (row%2!=0 && column>=1 && column < hidden_len-1 && direction==0){
+         plateau_[row][column]->place();
+         plateau_[row][column-1]->place();
+         plateau_[row][column+1]->place();
+     }else if(column%2!=0 && row>=1 && row< hidden_len-1 && direction==1){
+         plateau_[row][column]->place();
+         plateau_[row+1][column]->place();
+         plateau_[row-1][column]->place();
+      }else{
+          throw QuoridorExceptions(1,"wall wrongly placed",1);
+      }
 }
+
 void Board::place(unsigned row, unsigned column){
         plateau_[row][column]->place();
 }
